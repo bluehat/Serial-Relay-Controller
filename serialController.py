8912d5cd37ebad75788cf9dc44eb2ff9adbce64e
bluehat <bluehat@Matapan.(none)>
2010-07-25 22:48:38 -0700

    screw it, might as well put the whole maglock client in here

--- serialController.py
+++ serialController.py
@@ -1,19 +1,40 @@
 import serial
 import time
+import urllib
+import json
 
 def relayOn():
   relayfile.setDTR(True)
-  print 'on'
+  #print 'on'
 
 def relayOff():
   relayfile.setDTR(False)
-  print 'off'
+  #print 'off'
 
 relayfile = serial.Serial('/dev/ttyUSB0', baudrate=9600)
 
+def getUsers():
+  userURL = urllib.urlopen('http://signup.hackerdojo.com/api/rfid')
+  data = userURL.read()
+  userURL.close()
+  userData = json.loads(data)
+  return userData
+
+userData = getUsers()
+for user in userData:
+  print user['rfid_tag']
+
 while True:
-  relayOn()
-  time.sleep(1)
-  relayOff()
-  time.sleep(1)
+  key = raw_input('RFID?').strip()
+  print key
+  for user in userData:
+     print user['rfid_tag']
+     if key == user['rfid_tag']:
+        foundUser = user
   
+  if foundUser is not None:
+    print 'hello ' + foundUser['username']
+    relayOff()
+    time.sleep(3)
+    relayOn()
+    foundUser = None
