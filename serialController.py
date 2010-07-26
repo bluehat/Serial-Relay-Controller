d821e87e4bdae6e1ae9256cb3dce7b7183ff11dd
bluehat <bluehat@Matapan.(none)>
2010-07-25 21:34:12 -0700

    and now it works

--- serialController.py
+++ serialController.py
@@ -1,1 +1,19 @@
-print 'first commit'
+import serial
+import time
+
+def relayOn():
+  relayfile.setDTR(True)
+  print 'on'
+
+def relayOff():
+  relayfile.setDTR(False)
+  print 'off'
+
+relayfile = serial.Serial('/dev/ttyUSB0', baudrate=9600)
+
+while True:
+  relayOn()
+  time.sleep(1)
+  relayOff()
+  time.sleep(1)
+  
